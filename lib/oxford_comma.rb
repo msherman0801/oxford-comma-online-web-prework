e4f2978693cb944dd52aa3eb0fddba0c60d154ce
Matthew Sherman <msherman0801@gmail.com>
2018-10-25 19:48:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,9 @@ def oxford_comma(array)
   
   if array.length == 2 
     array.join(' and ')
+  elsif array.length > 2 
+    third = array.pop
+    array.delete(array.last)
+    array.push("and #{third}")
+    array.join(', ')
 end
