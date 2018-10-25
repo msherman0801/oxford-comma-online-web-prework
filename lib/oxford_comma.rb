dad5559bcab9f6f879008faf1d7a2a4ab4333763
Matthew Sherman <msherman0801@gmail.com>
2018-10-25 19:49:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
-  
-  if array.length == 2 
+  if array.length == 1 
+    array.pop
+  elsif array.length == 2 
     array.join(' and ')
   elsif array.length >= 3
     third = array.pop
